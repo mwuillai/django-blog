ae8a1fc9da1fb8729d1c027bfa822b22997011a2
Mathieu WUILLAI <mathieu.wuillai@oxatis.com>
2018-08-23 22:40:48 +0200

    evolution de l'inscription

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,25 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class Users(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    email = models.EmailField(blank=True, null=True)
+class Profile(models.Model):
+    """Mettre à jour cette classe pour enrichir la classe par défaut de django
+    plutôt que de créer une nouvelle table utilisateur"""
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
     newsletter = models.BooleanField(default=False)
     registration_date = models.DateTimeField(auto_now_add=True)
+    bio = models.CharField(max_length=30, blank=True)
+    birth_date=models.DateField(null=True, blank=True)
 
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 class Articles(models.Model):
     title = models.CharField(max_length=200)
