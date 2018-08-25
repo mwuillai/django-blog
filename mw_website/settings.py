4956c44dbc297d8944d2cbb2ca012983a050f973
mwuillai <wuillaimathieu78@gmail.com>
2018-08-25 11:08:37 +0200

    MAJ settings pour windows

--- mw_website/settings.py
+++ mw_website/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'user1',
-        'PASSWORD': 'aqwzsx',
+        'USER': 'postgres',
+        'PASSWORD': 'aqwZSX',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
